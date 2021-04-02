4465ba541eb84a5daf85ef66a9f2d9f4abf9e4b1
cchearn <cchearn.williamp@gmail.com>
2021-04-01 20:16:06 -0400

    Noah added script for cocktail api population

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var searchForm = $('#searchForm');
-var searchInput = document.querySelector("#searchInput")
+var searchInput = document.querySelector("#searchInput");
 var recipeCardsToggler = document.querySelector("#recipeCardsToggler");
 var coctailCardsToggler = document.querySelector("#coctailCardsToggler");
+var landingPageToggler = document.querySelector("#landingPage");
 var nameOne = document.querySelector('#nameOne');
 var nameTwo = document.querySelector('#nameTwo');
 var nameThree = document.querySelector('#nameThree');
@@ -20,6 +21,17 @@ var recipeLinkThree = document.querySelector("#recipeLinkThree");
 var winePairOne = document.querySelector("#winePairOne");
 var winePairTwo = document.querySelector("#winePairTwo");
 var winePairThree = document.querySelector("#winePairThree");
+
+//code for the cocktail API
+var cocktailOneName = document.querySelector('#cocktailNameOne');
+var cocktailTwoName = document.querySelector('#cocktailNameTwo');
+var cocktailThreeName = document.querySelector('#cocktailNameThree');
+var cocktailOneImg = document.querySelector('#cocktailOneImg');
+var cocktailTwoImg = document.querySelector('#cocktailTwoImg');
+var cocktailThreeImg = document.querySelector('#cocktailThreeImg');
+var cocktailOneInstructions = document.querySelector('#cocktailOneInstructions');
+var cocktailTwoInstructions = document.querySelector('#cocktailTwoInstructions');
+var cocktailThreeInstructions = document.querySelector('#cocktailThreeInstructions');
 
 // Showing the first slide and creating a variable to use in other functions to change slides
 var slideIndex = 1;
@@ -75,11 +87,14 @@ function inputHandler(event) {
     return;
   } else {
       getApi();
+      getCocktailOneApi(); 
+      getCocktailTwoApi();
+      getCocktailThreeApi();
   }
 
  async function getApi() {
     // calls the API
-     var requestUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=ab9e9f4b6c1845aabcce129dd18aa63b&query=' + userSearch;
+     var requestUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=ab9e9f4b6c1845aabcce129dd18aa63b&query=' + userSearch + " dinner";
      
     fetch(requestUrl)
       .then(function (response) {
@@ -87,9 +102,12 @@ function inputHandler(event) {
       })
       .then(function (data) {
         console.log(data);
+
         // changes the recipe and coctail cards from hidden to displayed
+        landingPageToggler.setAttribute("style", "display: none");
         recipeCardsToggler.setAttribute("style", "display: block");
         coctailCardsToggler.setAttribute("style", "display: block");
+
         // stores the recipe ID for the second API call
         recipeID1 = data.results[0].id;
         recipeID2 = data.results[1].id;
@@ -102,19 +120,19 @@ function inputHandler(event) {
         nameOne.textContent = data.results[0].title;
         imgOne.setAttribute("height", 300);
         imgOne.setAttribute("width", 300);
-        imgOne.setAttribute("style", "border-radius: 20px");
+        imgOne.setAttribute("style", "border-radius: 40px");
         imgOne.setAttribute("src", data.results[0].image);
 
         nameTwo.textContent = data.results[1].title;
         imgTwo.setAttribute("height", 300);
         imgTwo.setAttribute("width", 300);
-        imgTwo.setAttribute("style", "border-radius: 20px");
+        imgTwo.setAttribute("style", "border-radius: 40px");
         imgTwo.setAttribute("src", data.results[1].image);
 
         nameThree.textContent = data.results[2].title;
         imgThree.setAttribute("height", 300);
         imgThree.setAttribute("width", 300);
-        imgThree.setAttribute("style", "border-radius: 20px");
+        imgThree.setAttribute("style", "border-radius: 40px");
         imgThree.setAttribute("src", data.results[2].image);
         
         // invokes second API call for more specific information using the recipe ID
@@ -140,10 +158,12 @@ function inputHandler(event) {
       })
       .then(function (data2) {
         console.log(data2);
+
         // adds descriptions and links to the recipe cards
         descriptionOne.innerHTML = data2[0].summary;
         descriptionTwo.innerHTML = data2[1].summary;
         descriptionThree.innerHTML = data2[2].summary;
+
         recipeLinkOne.setAttribute("href", data2[0].sourceUrl);
         recipeLinkOne.textContent = data2[0].sourceUrl;
         recipeLinkTwo.setAttribute("href", data2[1].sourceUrl);
@@ -164,5 +184,69 @@ function inputHandler(event) {
         
     } 
 }
+
+function getCocktailOneApi() {
+
+  var cocktailRequestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+  
+  fetch(cocktailRequestUrl)
+  .then (function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+     //Populates the cocktail cards with results from API call    
+    cocktailOneName.textContent = data.drinks[0].strDrink;
+    cocktailOneImg.setAttribute("height", 300);
+    cocktailOneImg.setAttribute("width", 300);
+    cocktailOneImg.setAttribute("style", "border-radius: 40px");
+    cocktailOneImg.setAttribute("src", data.drinks[0].strDrinkThumb);
+    cocktailOneInstructions.textContent = data.drinks[0].strIngredient1 + ", " + data.drinks[0].strIngredient2 + ", " + data.drinks[0].strIngredient3 + ", " + data.drinks[0].strIngredient4 + ", " + data.drinks[0].strIngredient5 + ", " + data.drinks[0].strIngredient6 + ", " + data.drinks[0].strIngredient7 + ", " + data.drinks[0].strIngredient8 + ", " + data.drinks[0].strIngredient9 + ", " + data.drinks[0].strIngredient10;
+  
+  })
+  }
+  function getCocktailTwoApi() {
+  
+    var cocktailRequestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    
+    fetch(cocktailRequestUrl)
+    .then (function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+       //Populates the cocktail cards with results from API call    
+      cocktailTwoName.textContent = data.drinks[0].strDrink;
+      cocktailTwoImg.setAttribute("height", 300);
+      cocktailTwoImg.setAttribute("width", 300);
+      cocktailTwoImg.setAttribute("style", "border-radius: 40px");
+      cocktailTwoImg.setAttribute("src", data.drinks[0].strDrinkThumb);
+      cocktailTwoInstructions.textContent = data.drinks[0].strIngredient1 + ", " + data.drinks[0].strIngredient2 + ", " + data.drinks[0].strIngredient3 + ", " + data.drinks[0].strIngredient4 + ", " + data.drinks[0].strIngredient5 + ", " + data.drinks[0].strIngredient6 + ", " + data.drinks[0].strIngredient7 + ", " + data.drinks[0].strIngredient8 + ", " + data.drinks[0].strIngredient9 + ", " + data.drinks[0].strIngredient10;
+    })
+    }
+    function getCocktailThreeApi() {
+  
+      var cocktailRequestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+      
+      fetch(cocktailRequestUrl)
+      .then (function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+         //Populates the cocktail cards with results from API call    
+        cocktailThreeName.textContent = data.drinks[0].strDrink;
+        cocktailThreeImg.setAttribute("height", 300);
+        cocktailThreeImg.setAttribute("width", 300);
+        cocktailThreeImg.setAttribute("style", "border-radius: 40px");
+        cocktailThreeImg.setAttribute("src", data.drinks[0].strDrinkThumb);
+        cocktailThreeInstructions.textContent = data.drinks[0].strIngredient1 + ", " + data.drinks[0].strIngredient2 + ", " + data.drinks[0].strIngredient3 + ", " + data.drinks[0].strIngredient4 + ", " + data.drinks[0].strIngredient5 + ", " + data.drinks[0].strIngredient6 + ", " + data.drinks[0].strIngredient7 + ", " + data.drinks[0].strIngredient8 + ", " + data.drinks[0].strIngredient9 + ", " + data.drinks[0].strIngredient10;
+      })
+      }
+  
+  // getCocktailOneApi(); 
+  // getCocktailTwoApi();
+  // getCocktailThreeApi();
+
 // Invokes inputHandler on user submission
 searchForm.on('submit', inputHandler);
